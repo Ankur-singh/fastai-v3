5f97e5ad8cc7664f3b66d62bc373d1a662686961
Ankur-singh <as.ankursingh3.1@gmail.com>
2019-05-07 18:17:20 +0530

    final.pkl

--- app/server.py
+++ app/server.py
@@ -8,12 +8,11 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-# export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
 # export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+# export_file_name = 'export.pkl'
 
-export_file_url = 'https://drive.google.com/file/d/1_OSD7XQxUFeuztBJ9iPEInpHNyAiSrcl/view?usp=sharing'
-export_file_name = 'final_model_exported'
+export_file_url = 'https://drive.google.com/file/d/1b9LyUGaGazjlDS4ysEiMQJwdiHG98pAK/view?usp=sharing'
+export_file_name = 'final.pkl'
 
 # classes = ['black', 'grizzly', 'teddys']
 classes = ['affenpinscher','afghan_hound', 'african_hunting_dog', 'airedale', 'american_staffordshire_terrier', 'appenzeller', 'australian_terrier',
@@ -64,7 +63,7 @@ loop.close()
 @app.route('/')
 def index(request):
     html = path/'view'/'index.html'
-    return HTMLResponse(html.open().read())
+    return HTMLResponse(html.open(encoding='utf-8').read())
 
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
